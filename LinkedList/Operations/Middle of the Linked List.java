ed2dd13eabf4aa8f62d5befa19df47bd55f667d8
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-08-21 01:22:03 -0700

    Create Middle of the Linked List.java

---
+++ LinkedList/Operations/Middle of the Linked List.java
@@ -0,0 +1,22 @@
+// Middle of the Linked List
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+// Slow-fast Approach 
+// Time complexity: O(n)
+// Space complexity:O(1)
+// Leetcode problem Link https://leetcode.com/problems/middle-of-the-linked-list/solutions/3827431/fast-and-slow-pointers-5-lines-easy-to-read-java/
+
+class Solution {
+
+    // Finding middle node using slow fast approach
+    public ListNode findMid(ListNode head){
+        ListNode slow = head; //+1
+        ListNode fast = head; //+2
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //slow is the midNode
+    }
+}
