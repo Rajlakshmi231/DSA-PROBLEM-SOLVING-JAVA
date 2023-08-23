23e99d98149972981f1dfcd31af4cc8d3e25497b
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-08-23 00:43:34 -0700

    Create DoublyLinkedList.java

---
+++ LinkedList/Operations/DoublyLinkedList.java
@@ -0,0 +1,75 @@
+// Doubly Linked List
+// What is Doubly Linked List?
+// A doubly linked list (DLL) is a special type of linked list in which each node contains a pointer to the previous node 
+// as well as the next node of the linked list.
+
+//  A node can be inserted in a Doubly Linked List in four ways:
+  //1. At the front of the DLL. 
+  //2. In between two nodes
+    //i. After a given node.
+    //ii. Before a given node.
+  //3. At the end of the DLL.
+
+public class DoubleLL{
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+        
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    
+    // AddFirst
+    // step1:- Create new Node
+    // step2:- newNode.next = head;
+    // step3:- head.prev = newNode;
+    // step4:- head = newNode;
+ 
+    public void addFirst(int data){      //Time Complexity: O(1)
+        Node newNode = new Node(data);
+        size++;
+        
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+  
+    // print function
+    public void print(){
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+         System.out.println("null");
+    }
+
+  
+    public static void main(String args[]){
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        
+        dll.print();
+        System.out.println(dll.size);
+        
+    }
+}
+
+// Output:-
+// 1<->2<->3<->null
+// 3
+  
+
