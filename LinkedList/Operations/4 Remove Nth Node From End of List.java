ca4e4d4d542741c2e78a30e4a89f20dae0396a64
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-08-26 00:14:23 -0700

    Update 4 Remove Nth Node From End of List.java

--- LinkedList/Operations/4 Remove Nth Node From End of List.java
+++ LinkedList/Operations/4 Remove Nth Node From End of List.java
@@ -2,6 +2,7 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
 // Iterative Approach
 
+// Approach:- 1
 // Time complexity:
 // O(N) for the loop to calculate the sz of the LL
 // O(N) for searching the position of the Node in while loop
@@ -22,9 +23,9 @@ class LinkedList {
             head = head.next; //RemoveFirst (one node is given)
             return head;
         }
-      // prev is present at size -n
+      // prev is present at size - n
         int i = 1;
-        int iToFind = size -n; 
+        int iToFind = size - n; 
         ListNode prev = head;
         while(i < iToFind){
             prev = prev.next;
@@ -34,3 +35,29 @@ class LinkedList {
         return head;
     }
 }
+
+
+// Approach:-2 In one traversal (same t.c but in one traversal)
+class LinkedList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      ListNode slow = head;
+      ListNode fast = head;
+      for(int i = 0; i <= n; i++){ //till the nth position which we want to delete
+        fast = fast.next;
+      }
+      if(fast == null){  //if we reach the nth node and fast is null at this point then nth node which we want to is the head node.
+        head = head.next;
+        return head;
+      }
+
+      while(fast.next != null){
+        slow = slow.next;
+        fast = fast.next;
+      }
+      // return slow finding the nth node
+      slow.next = slow.next.next; //deleting the node
+      return head;  
+    }
+}
+
+
