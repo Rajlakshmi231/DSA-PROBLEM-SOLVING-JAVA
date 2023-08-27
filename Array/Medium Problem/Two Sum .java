5c9f9ca4a11f0f643476771ed7d55fc1483573e3
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-08-26 21:04:06 -0700

    Create Two Sum .java

---
+++ Array/Medium Problem/Two Sum .java
@@ -0,0 +1,34 @@
+// // Two Sum : Check if a pair with given sum exists in Array
+
+// Problem Statement: Given an array of integers arr[] and an integer target.
+// 1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+// 2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+// Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+// Naive Approach(Brute-force approach): 
+import java.util.*;
+public class Solution {
+    public static String twoSum(int n, int []arr, int target) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+    }
+
+ }
+  
+// Output: This is the answer for variant 1: YES
+
+// Time Complexity: O(N2), where N = size of the array.
+// Reason: There are two loops(i.e. nested) each running for approximately N times.
+// Space Complexity: O(1) as we are not using any extra space.
