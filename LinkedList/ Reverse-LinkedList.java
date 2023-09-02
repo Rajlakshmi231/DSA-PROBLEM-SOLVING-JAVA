60a4cbd34b9986d75c2c18e54273f31c23d986e2
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-01 22:16:38 -0700

    Reverse-LinkedList.java

--- LinkedList/ Reverse-LinkedList.java
+++ LinkedList/ Reverse-LinkedList.java
@@ -1,8 +1,10 @@
 // Reverse Linked List 
 // Problem Statement:- Given the head of a singly linked list, reverse the list, and return the reversed list. 
 
-
+// Iterative Approach 
 // Time.Complexity:= O(N)
+// Space.Complexity:= O(1)
+
 class LinkedList {
     public ListNode reverseList(ListNode head) { 
         ListNode prev = null;
@@ -17,6 +19,29 @@ class LinkedList {
         } 
         head = prev; //Update head node 
         return head;  
+    }
+}
+
+
+
+// Recursive Approach
+// Time.Complexity:= O(N)
+// Space.Complexity:= O(N) stack space in each call
+
+class Solution {
+    public ListNode reverse(ListNode head){
+        // base case
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode smallHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return smallHead;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        return reverse(head);  
     }
 }
 
