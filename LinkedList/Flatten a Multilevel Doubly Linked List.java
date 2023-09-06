00cbfa1ed3a0e458520d8ff16ffa25e639167bd9
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-05 22:49:44 -0700

    Create Flatten a Multilevel Doubly Linked List.java

---
+++ LinkedList/Flatten a Multilevel Doubly Linked List.java
@@ -0,0 +1,25 @@
+// 430. Flatten a Multilevel Doubly Linked List
+
+class Solution {
+    public Node flatten(Node head) {
+        Node temp = head;
+        while(temp != null){
+            Node t = temp.next;
+            if(temp.child != null){
+                Node c = flatten(temp.child);
+                temp.next = c;
+                c.prev = temp;
+
+                //c ko age le jao
+                while(c.next != null){
+                    c = c.next;
+                }
+                c.next = t;
+                if(t != null)t.prev = c;
+            }
+            temp.child = null;
+            temp =t;
+        }
+        return head;
+    }
+}
