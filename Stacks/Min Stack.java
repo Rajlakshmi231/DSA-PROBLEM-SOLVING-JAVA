c2f7a177abe0f8bbcb534695178965b46277b308
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-14 23:33:55 -0700

    Min Stack.java

--- Stacks/Min Stack.java
+++ Stacks/Min Stack.java
@@ -37,3 +37,51 @@ class MinStack {
 
 
 // Approach 2   .C :- O(1)   S.C :-O(1) 
+
+class MinStack {
+    Stack<Long> st = new Stack<>();
+    long min = -1;
+
+    public MinStack() {    
+    }
+    
+    public void push(int val) {
+        long x = (long)val;
+        if(st.isEmpty()){
+            st.push(x);
+            min = x;
+        }
+        else if(x >= min){
+            st.push(x);
+        }
+        else if(x<min){
+            st.push(2*x - min); //we will push some fake value 
+            min = x;
+        }   
+    }
+    
+    public void pop() {
+        if(st.size() == 0) return;
+        else if(st.peek()>= min) st.pop();
+        else if(st.peek() < min){ //peek pr fake value padi h
+            //restore the old min
+            long oldMin = min;
+            min = 2*min - st.peek();
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        if(st.size() == 0) return -1;
+        long q = st.peek();
+        if(q>= min) return (int)q;
+        if(q< min) return (int)min;
+        return 0;
+    }
+    
+    public int getMin() {
+        if(st.size() == 0) return -1;
+        return (int)min;
+        
+    }
+}
