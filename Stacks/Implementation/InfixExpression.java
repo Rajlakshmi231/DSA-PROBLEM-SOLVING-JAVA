df8a9c6bc8d8459cafbea08d6a50a4a9954c3d6a
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-16 09:04:14 -0700

    InfixExpression.java

--- Stacks/Implementation/InfixExpression.java
+++ Stacks/Implementation/InfixExpression.java
@@ -2,18 +2,34 @@
 import java.util.*;
 public class infix{
     public static void main (String[] args){
-        String str = "9-5+3*4/6";
+        String str = "9-(5+3)*4/6";
         Stack<Integer> val = new Stack<>();
         Stack<Character> op = new Stack<>();
         for(int i=0; i<str.length(); i++){
             char ch = str.charAt(i);
+            
             int ascii = (int) ch;
             if(ascii >= 48 && ascii <=57){
                 val.push(ascii - 48);
             }
-            else if (op.size() == 0){
+                
+            else if (op.size() == 0|| ch == '(' || op.peek() == '('){
                 op.push(ch);
             }
+             else if(ch == ')'){
+                while(op.peek() != '('){
+                    // process  (two pop from val stack and one pop from optr stack then process it 
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    if(op.peek() == '-') val.push(v1-v2);
+                    if(op.peek() == '+') val.push(v1+v2);
+                    if(op.peek() == '*') val.push(v1*v2);
+                    if(op.peek() == '/') val.push(v1/v2);
+                    op.pop();
+                }
+                op.pop(); // then pop the opening brackets '(' we are looking for our corresponding closing brackets. 
+            }    
+                
             else{
                 if(ch == '+' || ch == '-'){
                     //work
@@ -33,7 +49,7 @@ public class infix{
                         int v2 = val.pop();
                         int v1 = val.pop();
                         if(op.peek() == '*' )val.push(v1*v2);
-                        if(op.peek() == '*' )val.push(v1/v2);
+                        if(op.peek() == '/' )val.push(v1/v2);
                         op.pop();
                         //push
                         op.push(ch);
@@ -55,4 +71,7 @@ public class infix{
         System.out.println(val.peek());
     }
 }
+
+// Output:- 4
+
   
