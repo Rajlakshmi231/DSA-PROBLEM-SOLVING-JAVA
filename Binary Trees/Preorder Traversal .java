8472cdfb128d0d74d04e3bdc3446994211e0f40b
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-19 08:20:43 -0700

    Create Preorder Traversal .java

---
+++ Binary Trees/Preorder Traversal .java
@@ -0,0 +1,23 @@
+// Preorder Traversal
+// T.C :- O(N)
+
+// Processing Order In Preorder Traversal (follow these process recursively)
+//1) root
+//2) left subtree
+//3) right subtree
+
+class Solution {
+    public void preorder( TreeNode root,List<Integer> ans){
+        if(root == null){
+            return ;
+        }
+        ans.add(root.val);
+        preorder(root.left, ans);
+        preorder(root.right, ans);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preorder(root, ans);
+        return ans;   
+    }
+}
