3e686af504cadf5873146d3795b74b043bfaba03
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-22 08:56:41 -0700

    Subtree Of Another Tree.java

--- Binary Trees/Subtree Of Another Tree.java
+++ Binary Trees/Subtree Of Another Tree.java
@@ -1,13 +1,21 @@
 // Subtree Of Another Tree
 
+// T.C :- O(m*n) n is the total number of nodes in the first binary tree and m is the total number of nodes in the subtree.
+// S.C :- O(min(n,m)) depth of recursion tree can go upto n or m, whichever is smaller.
+
 class Solution {
     public boolean isIdentical(TreeNode root, TreeNode subRoot){
+    //return true if both are null because it means we reached the leaf node at same time
+
         if(root == null && subRoot == null){
             return true;
         }
-        else if(root == null || subRoot == null || root.data != subRoot.data){
+        //return false if we don't reach leaf node at same time and the values are not same 
+        else if(root == null || subRoot == null || root.val != subRoot.val){
             return false;
         }
+
+        //if the value of current node are same then we check for left and right part
 
         if(!isIdentical(root.left, subRoot.left)){
             return false;
@@ -24,13 +32,13 @@ class Solution {
             return false;
         }
 
-        //compare root.data with subroot.data if both data are same then we check for Identical nodes using helper function
+        //compare root.val with subroot.val if both data are same then we check for Identical nodes using helper function
         if(root.val == subRoot.val){ 
             if(isIdentical(root, subRoot)){
                 return true;
             }
         }
-        //chech in left and right for subTree
-        return isSubtree(root.left, subRoot.left) ||isSubtree(root.right, subRoot.right) ;    
+        //use 'or' function because if subtree is present in any part, we return true
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);    
     }
 }
