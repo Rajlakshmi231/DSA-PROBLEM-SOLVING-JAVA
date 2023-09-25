f178d3b038b6a56ebb01bd0d6cacc8bcab49927b
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-09-25 08:21:05 -0700

    Create Implementation Of Stack Using Deque.java

---
+++ Queue/Implementation/Implementation Of Stack Using Deque.java
@@ -0,0 +1,37 @@
+// Implementation Of Stack Using Deque
+
+import java.util.* ;
+import java.io.*; 
+
+public class DequeDS {
+	// Initialize your data structure.
+	Deque<Integer> deque = new LinkedList<>();
+
+	// Pushes 'X' into the stack. Returns true if it gets pushed into the stack, and false otherwise.
+	public boolean push(int x) {
+		deque.addLast(x);
+		return true;
+	}
+
+	// Pops top element from Stack. Returns -1 if the stack is empty, otherwise returns the popped element.
+	public int pop() {
+		if(deque.isEmpty())  return -1;
+		else return deque.removeLast();
+	}
+
+	// Returns the topmost element of the stack. In case the stack is empty, it returns -1.
+	public int top() {
+		if(deque.isEmpty())  return -1;
+		else return deque.getLast();
+	}
+
+	// Returns true if the stack is empty, otherwise false.
+	public boolean isEmpty() {
+	  return deque.isEmpty();	
+	}
+
+	// Returns the number of elements currently present in the stack.
+	public int size() {
+		return deque.size();
+	}
+}
