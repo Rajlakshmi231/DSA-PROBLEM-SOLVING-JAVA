a55091bae67332a8211a99a9d67dd127351edc50
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-10-02 10:20:20 -0700

    Create Insert In Heap .java

---
+++ Heaps/Insert In Heap .java
@@ -0,0 +1,37 @@
+// Insert In Heap
+
+import java.util.*;
+public class Classroom {
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        public void add(int data){ //T.C O(logN)
+            //add at last idx
+            arr.add(data);
+            
+            int x = arr.size() - 1; //x iss child index
+            int par = (x - 1)/2; //par index
+            
+            while(arr.get(x) < arr.get(par)){
+                //swap
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+                
+                x = par;
+                par = (x - 1)/2;
+                
+            }
+            
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+    }
+}
