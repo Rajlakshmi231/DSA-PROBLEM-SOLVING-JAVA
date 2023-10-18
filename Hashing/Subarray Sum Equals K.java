2d4f5c7e47778be322373248b7e888e05925cd54
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-10-18 08:14:26 -0700

    Create Subarray Sum Equals K.java

---
+++ Hashing/Subarray Sum Equals K.java
@@ -0,0 +1,23 @@
+// Subarray Sum Equals K
+// T.C :- O(N)
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+    
+        int sum =0;
+        int ans =0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for(int j =0; j < nums.length; j++){
+            sum += nums[j]; //sum(j)
+
+            if(map.containsKey(sum - k)){
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum , 0) + 1);
+        }
+        return ans;
+    }
+}
