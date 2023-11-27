b45b827db4336afb667e3c323b3c786866e0a598
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-11-27 08:44:06 -0700

    Create  Missing Number.java

---
+++ Array/EasyProblems/Missing Number.java
@@ -0,0 +1,59 @@
+ // Missing Number
+// Summation Approach:-
+// T.C :- O(N), where N = size of array+1.
+// S.C :- O(1) as we are not using any extra space. 
+
+import java.util.*;
+
+public class tUf {
+    public static int missingNumber(int []a, int N) {
+
+        //Summation of first N numbers:
+        int sum = (N * (N + 1)) / 2;
+
+        //Summation of all array elements:
+        int s2 = 0;
+        for (int i = 0; i < N - 1; i++) {
+            s2 += a[i];
+        }
+
+        int missingNum = sum - s2;
+        return missingNum;
+    }
+
+    public static void main(String args[]) {
+        int N = 5;
+        int a[] = {1, 2, 4, 5};
+
+        int ans = missingNumber(a, N);
+        System.out.println("The missing number is: " + ans);
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// XOR Approach :-
+// T.C :- O(N), where N = size of array+1.
+// S.C :- O(1) as we are not using any extra space.   
+  
+import java.util.*;
+public class tUf {
+    public static int missingNumber(int []a, int N) {
+
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            xor2 = xor2 ^ a[i]; // XOR of array elements
+            xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+        }
+        xor1 = xor1 ^ N; //XOR up to [1...N]
+
+        return (xor1 ^ xor2); // the missing number
+    }
+
+    public static void main(String args[]) {
+        int N = 5;
+        int a[] = {1, 2, 4, 5};
+
+        int ans = missingNumber(a, N);
+        System.out.println("The missing number is: " + ans);
+    }
+}
