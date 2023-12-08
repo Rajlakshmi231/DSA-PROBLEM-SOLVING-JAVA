6b16be6aab2b9fc08c44b08f2117c0b7fd036e9b
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-08 08:36:51 -0700

    Update Search a 2D Matrix.java

--- 2-D ARRAY/Search a 2D Matrix.java
+++ 2-D ARRAY/Search a 2D Matrix.java
@@ -16,3 +16,31 @@ class Solution {
         return false;
     }
 }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Binary Search Approach
+// T.C :- O(log(NxM))
+// S.C :- O(1)     
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row =matrix.length, col = matrix[0].length;
+        int start = 0 ; 
+        int end = row*col - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int element = matrix[mid/col][mid%col];
+            if(element == target){
+                return true;
+            }
+            else if(target > element){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}    
