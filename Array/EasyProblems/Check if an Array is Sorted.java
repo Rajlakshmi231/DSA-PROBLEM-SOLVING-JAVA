389cdf82e6d9ecf74bd2985c3401bab8aeb1b0bd
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-11 10:29:35 -0700

    Update Check if an Array is Sorted.java

--- Array/EasyProblems/Check if an Array is Sorted.java
+++ Array/EasyProblems/Check if an Array is Sorted.java
@@ -1,4 +1,5 @@
-//Brute force  T.C: O(N^2) ,S.C: O(1)
+// Check if an Array is Sorted
+//Brute force  T.C: O(N^2),  S.C: O(1)
 
 public class Main {
   public static boolean isSorted(int arr[], int n) {
@@ -8,13 +9,11 @@ public class Main {
           return false;
       }
     }
-
     return true;
   }
 
   public static void main(String args[]) {
     int arr[] = {1, 2, 3, 4, 5}, n = 5;
-
     System.out.println(isSorted(arr, n));
   }
 }
