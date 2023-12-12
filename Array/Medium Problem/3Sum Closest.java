2f8143778f30ec67fa9dd224bbb51a4584b345db
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-12 07:59:30 -0700

    Create 3Sum Closest.java

---
+++ Array/Medium Problem/3Sum Closest.java
@@ -0,0 +1,26 @@
+// 3Sum Closest
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        int closestSum = 100000;
+        Arrays.sort(nums);
+
+        for(int k =0; k<= n-3; k++){
+            int i =k+1;
+            int j = n-1;
+
+            while(i < j){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(Math.abs(target - sum) < Math.abs(target - closestSum)){
+                    closestSum = sum;
+                }
+                if(sum < target)
+                    i++;
+                else
+                    j--;    
+            }
+        }
+        return closestSum;
+    }
+}
