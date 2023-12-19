677054f9a63ea59e90e6c630dde072d82cca02d1
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-18 23:28:42 -0700

    Update String Compression.java

--- String/String Compression.java
+++ String/String Compression.java
@@ -1,4 +1,32 @@
-// String Compression
+// String Compression Type 1
+
+import java.util.*;
+public class Classroom{
+    
+    public static String compression1(String s){
+        String result = s.charAt(0) + ""; //s.substring(0,1); //or we can also string from charAt eg:- s.charAt(0) + "";
+        
+        for(int i =1; i<s.length(); i++){
+            char curr = s.charAt(i);
+            char prev = s.charAt(i-1);
+            if( curr != prev){
+                result += curr;
+            }
+        }
+        return result;
+    }
+    
+    public static void main (String args[]){
+        String s = "aaabbcccddaeef";
+        System.out.println(compression1(s));
+        
+    }
+}
+// Output :- abcdaef
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// String Compression Type 1
 // T.C :- O(n)
 // S.C :- O(1)
 
