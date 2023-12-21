84f88ba1390cc81368d38f1cf8d2eb2dbf8abca6
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-21 00:12:51 -0700

    Update Valid Anagram.java

--- String/Valid Anagram.java
+++ String/Valid Anagram.java
@@ -1,5 +1,7 @@
  // Valid Anagram
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+//APPROACH 1 :-
 // T.C :- O(nlogn) since sorting function requires nlogn iterations.
 // S.C :- O(1)
 
@@ -33,4 +35,34 @@ public static void main(String args[]){
   }
 }
 
-// Output :- True
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//APPROACH 2 (Using HashMap) :-
+// T.C :- O(N) 
+// S.C :- O(N)
+ 
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i =0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        for(int i =0; i<t.length(); i++){
+            char ch = t.charAt(i);
+         
+            if(map.get(ch) != null){
+                if(map.get(ch) == 1){
+                    map.remove(ch); //if freq is 1 direct remove it
+                
+                else{
+                    map.put(ch, map.get(ch) - 1); //if freq is > 1 decrese the freq by 1
+                }
+            }
+            else{
+                return false; 
+            }
+        }
+        return map.isEmpty(); //if map is empty at last then its valid if not then not valid  
+    }
+} 
