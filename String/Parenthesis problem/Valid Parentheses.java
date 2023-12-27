8557ff942fcd5c492e962069a73a539ed398b051
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-27 07:13:46 -0700

    Create Valid Parentheses.java

---
+++ String/Parenthesis problem/Valid Parentheses.java
@@ -0,0 +1,35 @@
+// Valid Parentheses
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            // Opening brackets
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                // closing brackets
+                if(st.isEmpty()){
+                    return false;
+                }
+                if((st.peek() =='('  && ch == ')') //()
+                ||(st.peek() =='{'  && ch == '}') //{}
+                ||(st.peek() =='['  && ch == ']')){ //[]
+                st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }   
+    }
+}
