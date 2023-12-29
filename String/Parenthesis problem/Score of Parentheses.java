9f0ebc9b4cef6c55459768392c4de17e5b6d064e
RajLakshmi <107094938+Rajlakshmi231@users.noreply.github.com>
2023-12-29 08:47:48 -0700

    Create Score of Parentheses.java

---
+++ String/Parenthesis problem/Score of Parentheses.java
@@ -0,0 +1,24 @@
+// Score of Parentheses
+
+class Solution {
+    int i =0;
+    public int scoreOfParentheses(String s) {
+        int score =0;
+        while(i<s.length()){
+            char first = s.charAt(i); i++;
+            if(first == '('){
+                if(s.charAt(i) == ')'){
+                    score++;
+                    i++;
+                }
+                else{
+                    score = score + 2*(scoreOfParentheses(s));
+                }
+            }
+            else{
+                return score;
+            }
+        }
+        return score;
+    }
+}
